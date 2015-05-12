49bba3c732d79e56c78a7b8844300bfd759c3d54
Jason A Belsky <jabelsky@gmail.com>
2015-05-12 14:17:16 -0400

    Added CMDLineMain

---
+++ chip_seq_analysis/src/chip_seq_analysis/CMDLineMain.java
@@ -0,0 +1,30 @@
+package chip_seq_analysis;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class CMDLineMain {
+
+	public static void main(String[] args) throws IOException {
+
+		// Get the program to execute
+		String java_executable_program = args[0];
+		
+		// Get the command line arguments
+		String[] cmd_line_args = Arrays.copyOfRange(args, 1, args.length);
+		
+		// Choose the program to run based on the args[0];
+		if(java_executable_program.equals("ChIPSeqFeatureDensitySignal")){
+			System.out.println("Executing ChIPSeqFeatureDensitySignal!");
+			ChIPSeqFeatureDensitySignal.main(cmd_line_args);
+		}else if(java_executable_program.equals("ChIPSeqFeatureSignal")){
+			System.out.println("Executing ChIPSeqFeatureSignal!");
+			ChIPSeqFeatureSignal.main(cmd_line_args);
+		}else{
+			System.out.println("Did not specify either 'ChIPSeqFeatureDensitySignal' " +
+							   "or 'ChIPSeqFeatureSignal' as first argument!");
+		}
+
+	}
+
+}
